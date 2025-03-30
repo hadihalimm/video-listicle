ff9fda0e603f253f4e85fc37ce9db77afa4e8702
Hadi Halim Kamil <hadihalimkamil@gmail.com>
2025-03-30 14:41:48 +0700

    implement homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,81 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useRef, useState } from 'react';
+import JsonInput, { VideoData } from './components/JsonInput';
+import { Button } from './components/ui/button';
+import ReactPlayer from 'react-player/lazy';
+import VideoCard from './components/VideoCard';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [jsonData, setJsonData] = useState<VideoData[] | null>(null);
+  const [videos, setVideos] = useState<VideoData[] | null>(null);
+  const [currentVideo, setCurrentVideo] = useState<VideoData | null>(null);
+  const playerRef = useRef<HTMLDivElement>(null);
+  const [playerSize, setPlayerSize] = useState({
+    width: '640px',
+    height: '360px',
+  });
+
+  useEffect(() => {
+    const updatePlayerSize = () => {
+      const width = window.innerWidth < 640 ? '100vw' : '640px';
+      const height = window.innerWidth < 640 ? '56.25vw' : '360px';
+      setPlayerSize({ width, height });
+    };
+
+    updatePlayerSize();
+    window.addEventListener('resize', updatePlayerSize);
+    return () => window.removeEventListener('resize', updatePlayerSize);
+  }, []);
+
+  const handlePlay = (video: VideoData) => {
+    setCurrentVideo(video);
+    setTimeout(() => {
+      playerRef.current?.scrollIntoView({
+        behavior: 'smooth',
+        block: 'center',
+      });
+    }, 100);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+    <main className="flex flex-col justify-center items-center mx-auto mt-16 gap-y-4 lg:max-w-7xl mb-8">
+      <h1 className="font-paytone text-center text-5xl">PlayListicle</h1>
+      <p className="mb-4 text-center">
+        Effortlessly create and showcase video listicles with dynamic playback
+        and seamless engagement
       </p>
-    </>
-  )
+      <JsonInput onJsonParsed={setJsonData} />
+      <Button
+        className="w-1/4"
+        disabled={!jsonData}
+        onClick={() => setVideos(jsonData)}>
+        Generate
+      </Button>
+
+      {currentVideo && (
+        <div ref={playerRef}>
+          <ReactPlayer
+            url={currentVideo?.videoUrl}
+            controls
+            playing
+            width={playerSize.width}
+            height={playerSize.height}
+          />
+        </div>
+      )}
+
+      {videos?.length && (
+        <div className="grid grid-cols-1 lg:grid-cols-3 gap-4">
+          {videos?.map((item, index) => (
+            <VideoCard
+              video={item}
+              key={index}
+              onPlay={() => handlePlay(item)}
+            />
+          ))}
+        </div>
+      )}
+    </main>
+  );
 }
 
-export default App
+export default App;
